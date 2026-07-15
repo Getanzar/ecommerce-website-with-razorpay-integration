981c2fc4a6a8b7e7885ff67333236b4fdb197deb
Anzar Ul Haq <ulhaqanzar444@gmail.com>
2026-07-15 21:24:29 +0530

    Add Render domain to ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -21,11 +21,15 @@ ALLOWED_HOSTS = [
     'localhost',
     'ziyamart.in',
     'www.ziyamart.in',
+    'ecommerce-website-with-razorpay.onrender.com',  # add this
 ]
+
 CSRF_TRUSTED_ORIGINS = [
     "https://ziyamart.in",
     "https://www.ziyamart.in",
+    "https://ecommerce-website-with-razorpay.onrender.com",  # add this
 ]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
@@ -40,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,13 +87,14 @@ DATABASES = {
 }
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
