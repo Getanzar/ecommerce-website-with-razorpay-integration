c7ef07632334c5fcda5b7e4669b102e089d5c2c8
Anzar Ul Haq <ulhaqanzar444@gmail.com>
2026-07-27 13:19:03 +0530

    Add email SMTP timeout

--- config/settings.py
+++ config/settings.py
@@ -129,11 +129,10 @@ DELHIVERY_PICKUP_LOCATION = "ziya garments bazaar wilson gunj pathantola road, s
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = f"ZIYAMART <{EMAIL_HOST_USER}>"
