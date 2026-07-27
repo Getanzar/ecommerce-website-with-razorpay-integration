e067e1aab04da240faad033823c89561e7bd9ed3
Anzar Ul Haq <ulhaqanzar444@gmail.com>
2026-07-27 15:27:15 +0530

    Configure Brevo SMTP

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_TIMEOUT = 10
+EMAIL_TIMEOUT = 60
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
